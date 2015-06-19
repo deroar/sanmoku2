36b81676580626a694f4dbe9d7b9d6d942068ed7
deroar <vagrant@localhost.localdomain>
2015-06-19 09:05:14 +0000

    add login

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express'),
-<<<<<<< HEAD
 	app = express(),
     bodyParser = require('body-parser'),
     morgan = require('morgan'),
@@ -10,17 +9,6 @@ var express = require('express'),
 
 var server = require('http').Server(app),
 	io = require('socket.io')(server);
-=======
-    bodyParser = require('body-parser'),
-    morgan = require('morgan'),
-    methodOverride = require('method-override'),
-    app = express(),
-    post = require('./routes/post')
-;
-
-app.set('views',__dirname + '/views');
-app.set('view engine','ejs');
->>>>>>> origin/master
 
 //middleware
 app.use(morgan({format: 'dev', immediate: true }));
@@ -28,7 +16,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride());
 
-<<<<<<< HEAD
 server.listen(3000);
 console.log("server starting...");
 
@@ -77,17 +64,3 @@ io.sockets.on('connection',function(socket){
 	});
 
 });
-=======
-//初期画面
-app.get('/',post.index);
-
-//○　or　×
-app.post('/pick',post.pick);
-
-
-//画面初期化
-app.post('/init',post.init);
-
-app.listen(3000);
-console.log("server starting...");
->>>>>>> origin/master
