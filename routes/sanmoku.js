d779557b2387f5dbcf3722bcace0d4b12190c056
deroar <fnorihisa@gmail.com>
2015-06-25 05:46:18 +0000

    3人目の処理をしない

--- routes/sanmoku.js
+++ routes/sanmoku.js
@@ -7,6 +7,7 @@ var turn = 0; //0が先攻○”,1が後攻"×"
 var input = ["○","×"]; //turnにより○か×を選択する
 
 var turnPlayer = ""; //turnPlayerの名前を格納
+var chkPlayer = [];
 
 var isRun = 1; //ゲーム中1、ゲーム終了0
 
@@ -15,27 +16,29 @@ exports.index = function(req,res){
 };
 
 exports.pick = function(req,res){
-	var formName = Object.keys(req.body); //req.bodyからkey部分を取り出す {a1:"○"}
+	var formName = Object.keys(req.body); //req.bodyからkey部分を取り出す {name: "username" , a1: "○"}
 
-	if( turnPlayer != eval("req.body." + formName[0])){
-		if(eval("req.body." + formName[1])== ""){  //postされたvauleの値（例：re.body.a1）がブランクであれば,処理をする
-			screen[formName[1]]=input[turn];
 
-			//判定
-			if(judge(turn,screen) != ""){
-				isRun = 0;
-				res.render('sanmoku/result',{result:input[turn]});
-			}else{
-				//ターンの入れ替え
-				if(turn == 0){
-					turn=1;
-				}else if(turn == 1){
-					turn=0;
-				}
-				turnPlayer = eval("req.body." + formName[0]);
-				res.render('sanmoku/index',{screen:screen});
+	if(chkPick(chkPlayer , req)){ //処理するかの条件チェック
+
+		screen[formName[1]]=input[turn];
+
+		//判定
+		if(judge(turn,screen) != ""){
+			res.render('sanmoku/result',{result:input[turn]});
+
+		}else{
+			//ターンの入れ替え
+			exchangeTurn();
+			//
+			if(chkPlayer.length < 2){
+				pushUsernmae(chkPlayer,eval("req.body." + formName[0]));
 			}
+
+			turnPlayer = eval("req.body." + formName[0]);
+			res.render('sanmoku/index',{screen:screen});
 		}
+
 	}else{ //postされたvauleの値がブランクでなければエラーとする
 		console.log("error");
 	}
@@ -80,4 +83,58 @@ var result = "";
 	}
 
 	return result;
+}
+
+//nameの重複チェック
+function isExists(array, value) {
+	  // 配列の最後までループ
+	  for (var i =0, len = array.length; i < len; i++) {
+	    if (value == array[i]) {
+	      // 存在したらtrueを返す
+	      return true;
+	    }
+	  }
+	  // 存在しない場合falseを返す
+	  return false;
+}
+//
+function pushUsernmae(array,value){
+
+	if(!isExists(array, value)){
+		array.push(value);
+	}
+	return true;
+}
+//処理をするかの条件チェック
+function chkPick(array , req){
+	var formName = Object.keys(req.body);
+
+	console.log("req > ",req);
+
+	//前回実行時のnameと同じ場合は処理しない
+	if(turnPlayer == eval("req.body." + formName[0])){
+		return false;
+	}
+
+	//クリックしたボタンの値がブランクでない場合は処理しない
+	if(eval("req.body." + formName[1]) != ""){
+		return false;
+	}
+
+	//3人目の人がボタンをクリックしても処理しない
+	if(array.length >= 2 && !isExists(array, eval("req.body." + formName[0]))){
+		return false;
+	}
+
+	return true;
+}
+//turnの交換
+function exchangeTurn(){
+
+	if(turn == 0){
+		turn=1;
+	}else if(turn == 1){
+		turn=0;
+	}
+
 }
